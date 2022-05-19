487130a0dffa458a2b25d8875699897aedd55a80
AdrianoM96 <adrym09@gmail.com>
2022-05-18 22:16:35 -0300

    Muevo de lugar el mock para tener codigo mas limpio

--- src/components/Items/ItemListContainer.js
+++ src/components/Items/ItemListContainer.js
@@ -1,69 +1,19 @@
 import React, { useState,useEffect } from "react";
 import ItemCount from "./ItemCount"
 import ItemList from "./ItemList";
-import zapas2 from '../assets/zapas-mujer1.jpg'
-import zapas3 from '../assets/zapas-puma-hombre.jpg'
-import buzo1 from '../assets/buzo-billabong-mujer.jpg'
-import pantalon1 from '../assets/pantalon-hombre.jpg'
+import { getProducts } from "../mock/AsyncMock";
 
 const ItemListContainer = (props) => {
   const [loading,setLoading] = useState(true)
     // eslint-disable-next-line no-unused-vars
     const [greeting,setGreeting] = useState(props.greeting)
     const [items,setItems] = useState([{}])
-    // eslint-disable-next-line no-unused-vars
-    const [itemsBd,setItemsBd] = useState([
-        {
-          id: 1,
-          title: 'Zapatillas puma',
-          description:'description',
-          image: zapas3,
-          category: 'hombre',
-          price: "$999"
-        },
-      {
-        id: 2,
-        title: 'Pantalon sport',
-        description:'description',
-        image: pantalon1,
-        category: 'hombre',
-        price: "$111"
-      },
-      {
-        id: 3,
-        title: 'Zapatillas sport',
-        description:'description',
-        image: zapas2,
-        category: 'mujer',
-        price: "$222"
-      },
-      {
-        id: 4,
-        title: 'Buzo top',
-        description:'description',
-        image: buzo1,
-        category: 'mujer',
-        price: "$444"
-      }
-    ])
-
+   
     function onAdd (){
       console.log("agrego al carrito")
   }
-
-      const  simuloRetraso = () => {   
-      
-        return new Promise((res, rej) => {
-          setTimeout(() => {
-            res(
-              itemsBd
-              );
-          }, 2000);
-        })
-      }
-    
       useEffect(() => {
-         simuloRetraso()
+         getProducts()
         .then((data) => {
           setItems(data);  
           setLoading(false)   
---
+++ src/components/mock/AsyncMock.js
@@ -0,0 +1,50 @@
+import zapas2 from '../assets/zapas-mujer1.jpg'
+import zapas3 from '../assets/zapas-puma-hombre.jpg'
+import buzo1 from '../assets/buzo-billabong-mujer.jpg'
+import pantalon1 from '../assets/pantalon-hombre.jpg'
+
+const itemsBd = [
+    {
+      id: 1,
+      title: 'Zapatillas puma',
+      description:'description',
+      image: zapas3,
+      category: 'hombre',
+      price: "$999"
+    },
+  {
+    id: 2,
+    title: 'Pantalon sport',
+    description:'description',
+    image: pantalon1,
+    category: 'hombre',
+    price: "$111"
+  },
+  {
+    id: 3,
+    title: 'Zapatillas sport',
+    description:'description',
+    image: zapas2,
+    category: 'mujer',
+    price: "$222"
+  },
+  {
+    id: 4,
+    title: 'Buzo top',
+    description:'description',
+    image: buzo1,
+    category: 'mujer',
+    price: "$444"
+  }
+]
+
+export const  getProducts = () => {   
+      
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(
+          itemsBd
+          );
+      }, 2000);
+    })
+  }
