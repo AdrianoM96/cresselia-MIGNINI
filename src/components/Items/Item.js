0538b974a53d691ce990205abce388dc6a5e91ed
AdrianoM96 <adrym09@gmail.com>
2022-05-20 22:16:39 -0300

    cambio menor

--- src/components/Items/Item.js
+++ src/components/Items/Item.js
@@ -15,7 +15,7 @@ const Item = ({product}) => {
                         />
                     <div className="card-body">
                         <h5 className="card-title">{product.title}</h5>
-                        <h5 className="card-title">{product.price}</h5>             
+                        <h5 className="card-title">${product.price}</h5>             
                     </div>
                 </Link>
             </div>
